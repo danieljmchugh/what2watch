d40f53a3de9af897725d1250dcaf6dca90fedb1f
Daniel <daniel@Daniels-MacBook-Air.local>
2022-03-20 13:32:36 +0100

    Added some middleware to backend and changed the http actions

--- backend/index.js
+++ backend/index.js
@@ -2,107 +2,117 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const { v4: uuidv4 } = require('uuid');
+
 
 
 const app = express();  
 app.use(express.json());
 app.use(cors());
+app.use(express.static('build'));
+const Note = require('./models/note');
 
-const Note = require('./models/note')
+/************************ Middleware ************************/ 
+const requestLogger = (request, response, next) => {
+    console.log(new Date());
+    console.log('Method: ', request.method);
+    console.log('Path: ', request.path);
+    console.log('Body: ', request.body);
+    console.log('---');
 
-let notes = [
-    {
-        id: 1,
-        content: "Hello there",
-        date: "2022-05-30T17:30:31.098Z"
-    },
-    {
-        id: 2,
-        content: "So uncivilized",
-        date: "2022-05-30T17:30:31.098Z"
-    },
-    {
-        id: 3,
-        content: "Only a Sith deals in absolutes",
-        date: "2022-05-30T17:30:31.098Z"
-    }
-]
-
-if (process.argv.length < 3) {
-    console.log('USE: node mongo.js <password>');
-    process.exit(1);
+    next();
 }
 
-const password = process.argv[2];
+app.use(requestLogger);
 
 /************************ GET ************************/ 
 
 //  Root
 app.get('/', (request, response) => {
-    console.log(new Date(), ' GET /');
+    
     response.send('<h1>WHAT2WATCH</h1>');
 })
 
 // Notes
 app.get('/api/notes', (request, response) => {
-    console.log(new Date(), ' GET /notes');
     Note.find({}).then(result => {
         response.json(result);
     })
 })
 
 // Note by ID
-app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id);
-    const note = notes.find(note => note.id === id);
-    
-    if (note) {
-        console.log(new Date(), ` GET notes/${note.id}`);
-        response.json(note);
-    } else {
-        response.status(404).end();
-    } 
+app.get('/api/notes/:id', (request, response, next) => {
+    // console.log(typeof(request.params.id));
+
+    Note.findById(request.params.id)
+        .then(result => {
+            if (result) {
+                response.send(result);
+            } else {
+                response.status(404).end();
+            }
+        })
+        .catch(error => {
+            next(error);
+        });
 })
 
 /************************ POST ************************/ 
 app.post('/api/notes',(request, response) => {
     
-    // Without the json-parser, the body property would be undefined
-    const body = request.body; 
-    
-    if (!body.content) {
-        return response.status(404).json({
-            error: "missing content"
-        })
-    }
+    const body = request.body
 
-    const note = {
+    if (body.content === undefined) {
+        return response.status(400).json({ error: 'content missing' })
+    }
+  
+    const note = new Note({
         content: body.content,
+        
         date: new Date(),
-        id: uuidv4()
-    }
-
-    notes = notes.concat(note);
-
-    console.log(new Date(), ' POST /notes', note);
-    response.json(note);
+    })
+  
+    note.save().then(savedNote => {
+        response.json(savedNote)
+    })
 })
 
 
 
 /************************ DELETE ************************/ 
 // Note by ID
-// TODO: Fix Delete as it cant find notes with uuidv4 id number
 app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id);
-    notes.filter(note => note.id !== id);
+    Note.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end();
+        })
+        .catch(error =>{
+            next(error);
+        });
 
-    console.log(new Date(), ` DELETE /notes/${request.params.id}`);
-    response.status(204).end()
 })
 
 
+
+
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error : 'unknown endpoint' });
+}
+
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    } 
+  
+    next(error)
+}
+
+
+app.use(unknownEndpoint);
+app.use(errorHandler); // this has to be the last loaded middleware.
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
